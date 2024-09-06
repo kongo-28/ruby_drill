36ab271c6e20090e96930ee13a4996a174401db6
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-06 10:44:28 +0900

    Create drill_13.rb

---
+++ drill_13.rb
@@ -0,0 +1,11 @@
+def missing_char(str, n)
+  str.slice!(n-1)
+  puts str
+end
+
+# 呼び出し例（1つ目の引数には対象となる文字列を、
+# 2つ目の引数には対象となる文字列から何番目の文字を削除するのかを指定しましょう）
+
+6.times do |i|
+missing_char('kitten', i+1)
+end
