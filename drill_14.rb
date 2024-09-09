ecdc1ce8f6e5cff3e4dcbc3bbd726ade9d009e2a
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-09 10:48:18 +0900

    drill14done

--- drill_14.rb
+++ drill_14.rb
@@ -1,13 +1,12 @@
 def police_trouble(a, b)
-  if a
-    puts a && !b
+  if (a && b) || (!a && !b)
+    puts "True"
   else
-    puts !a && b
+    puts "False"
   end
 end
-
 # 呼び出し例
 police_trouble(true, true) 
-# police_trouble(true, false) 
-# police_trouble(false, true) 
-# police_trouble(false, false)
+police_trouble(true, false) 
+police_trouble(false, true) 
+police_trouble(false, false)
