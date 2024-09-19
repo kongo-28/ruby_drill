89c236bf8e0f6f78fda114fdda948284b27f8103
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-19 10:51:02 +0900

    Create drill_34.rb

---
+++ drill_34.rb
@@ -0,0 +1,21 @@
+def close_far(a ,b ,c) 
+  if ((a-b).abs==1 || (a-c).abs==1) && (b-c).abs >=2
+    puts true
+  else
+    puts false
+  end
+end
+
+
+
+
+
+
+
+
+
+
+close_far(1, 2, 10) 
+close_far(1, 2, 3)
+close_far(4, 1, 3)
+
