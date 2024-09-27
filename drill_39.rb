adb9d7db99dc6e4bbe2be86d6c71eaa7372de9b4
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-27 10:33:53 +0900

    Create drill_39.rb

---
+++ drill_39.rb
@@ -0,0 +1,25 @@
+def bubble_sort(data)
+  length = data.length
+  for i in 0..length
+
+    count=0
+
+    for j in 0..length-2-i
+      if data[j] < data[j+1]
+        data[j], data[j+1] = data[j+1], data[j]
+        count +=1
+      end
+    end
+
+    if count==0
+      return data
+    end
+
+puts data.to_s
+  end
+end
+
+# 呼び出し例
+number = [1,23,4,333,6,12,45,79,5,6,84,8587,44,6,8,]
+bubble_sort(number)
+# puts number.to_s
